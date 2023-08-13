fd016a7eafb40a67bc869128702043fa267e7faf
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-13 21:43:37 +0300

    Installed Sass

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import './App.scss'
 import TakeawayPage from './Pages/TakeawayPage/TakeawayPage'
 
 function App() {
--- client/src/Pages/TakeawayPage/TakeawayPage.jsx
+++ client/src/Pages/TakeawayPage/TakeawayPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const TakeawayPage = () => {
     const [orderState, setOrderState] = useState('Waiting for confirmation')
@@ -8,6 +9,7 @@ const TakeawayPage = () => {
             <p>Explanation: This is a page which i need for another project, but since the master branch of this project is currently bugged and i cant work there, im using this one and then will transfer my code.</p>
             <h1>{orderState}</h1>
             <h6>{subOrderState}</h6>
+            <ProgressBar now={60}/>
         </div>
     )
 }
