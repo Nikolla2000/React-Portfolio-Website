3f8545529b2e75a446012f64477a8dbdf7288473
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-14 15:30:13 +0300

    Implemented React-Bootstrap in App.scss

--- client/src/Pages/TakeawayPage/TakeawayPage.jsx
+++ client/src/Pages/TakeawayPage/TakeawayPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import Button from 'react-bootstrap/Button';
 
 const TakeawayPage = () => {
     const [orderState, setOrderState] = useState('Waiting for confirmation')
@@ -9,7 +9,7 @@ const TakeawayPage = () => {
             <p>Explanation: This is a page which i need for another project, but since the master branch of this project is currently bugged and i cant work there, im using this one and then will transfer my code.</p>
             <h1>{orderState}</h1>
             <h6>{subOrderState}</h6>
-            <ProgressBar now={60}/>
+            <Button variant='danger'>Test</Button>
         </div>
     )
 }
