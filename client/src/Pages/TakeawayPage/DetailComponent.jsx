135a96d465c19b2a56ee620b85ee2b71253478f2
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-15 19:56:02 +0300

    Installed Font Awesome

--- client/src/Pages/TakeawayPage/DetailComponent.jsx
+++ client/src/Pages/TakeawayPage/DetailComponent.jsx
@@ -3,7 +3,7 @@ const DetailComponent = () => {
         <div className="detail-wrapper">
             <img src="#"/>
             <div className="details">
-                <p className="detail-name">Detail Name</p>
+                <p className="detail-title">Detail Title</p>
                 <p className="detail">Lorem ipsum dolor sin amet</p>
             </div>
         </div>
