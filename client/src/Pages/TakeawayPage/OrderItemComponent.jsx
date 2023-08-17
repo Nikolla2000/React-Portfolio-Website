fa43058ac95a47a989067b8e7321d53a5a486dd4
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-17 15:08:48 +0300

    Created "Contacts to the restaurant" section

--- client/src/Pages/TakeawayPage/OrderItemComponent.jsx
+++ client/src/Pages/TakeawayPage/OrderItemComponent.jsx
@@ -1,8 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone} from '@fortawesome/free-solid-svg-icons'
+
 const OrderItem = (props) => {
     return (
         <div className="item-wrapper">
-            <div className="item">{props.quantity > 1 && `${props.quantity}x `}{props.item}</div>
-            <div className="price">{props.totalPrice ? props.totalPrice : props.price * props.quantity} lv.</div>
+            <div className="item">
+                {props.quantity > 1 && `${props.quantity}x `}{props.item}
+                {props.subInfo && <p>{props.subInfo}</p>}
+            </div>
+            {props.subInfo ? <div className="contact-icon-wrapper"><FontAwesomeIcon icon={faPhone} /></div> 
+            :<div className="price">{props.totalPrice ? props.totalPrice : props.price * props.quantity} lv.</div>}
         </div>
     )
 }
--- client/src/Pages/TakeawayPage/TakeawayPage.jsx
+++ client/src/Pages/TakeawayPage/TakeawayPage.jsx
@@ -1,54 +1,60 @@
-    import { useEffect, useState } from "react"
-    import './TakeawayPageStyles.scss'
-    import DetailComponent from "./DetailComponent"
-    import details from "./orderDetails"
-    import ProgressBar from "./ProgressBar"
+import { useEffect, useState } from "react"
+import './TakeawayPageStyles.scss'
+import DetailComponent from "./DetailComponent"
+import details from "./orderDetails"
+import ProgressBar from "./ProgressBar"
 import OrderItem from "./OrderItemComponent"
 
-    const TakeawayPage = () => {
-        const [orderStatus, setOrderStatus] = useState('Waiting for confirmation')
-        const [subOrderStatus, setSubOrderStatus] = useState('Your order is currently waiting for confimation')
-        const [orderDetails, setOrderDetails] = useState(null)
-        const [orderItems, setOrderItems] = useState(null)
+const TakeawayPage = () => {
+    const [orderStatus, setOrderStatus] = useState('Waiting for confirmation')
+    const [subOrderStatus, setSubOrderStatus] = useState('Your order is currently waiting for confimation')
+    const [orderDetails, setOrderDetails] = useState(null)
+    const [orderItems, setOrderItems] = useState(null)
 
-        useEffect(() => {
-            setOrderDetails(details[0])
-            setOrderItems(details[1])
-        }, [])
+    useEffect(() => {
+        setOrderDetails(details[0])
+        setOrderItems(details[1])
+    }, [])
 
-        const totalOrderPrice = 
-            orderItems
-        ? orderItems.reduce((total, item) => total + item.price * item.quantity, 0)
-        : 0;
+    const totalOrderPrice = 
+        orderItems
+    ? orderItems.reduce((total, item) => total + item.price * item.quantity, 0)
+    : 0;
 
-        return (
-            <div className="takeaway-page-wrapper">
-                <p>Explanation: This is a page which i need for another project, but since the master branch of this project is currently bugged and i cant work there, im using this one and then will transfer my code.</p>
-                <h1>{orderStatus}</h1>
-                <h6>{subOrderStatus}</h6>
-                <ProgressBar status={orderStatus}/>
-                <div className="pizza-img">
-                    <img src='../../src/assets/images/menu-pizzas.png'/>
-                </div>
-                {orderDetails && orderDetails.map((orderDetail, index) => { 
-                    return (
-                        <DetailComponent 
-                            {...orderDetail}
-                            key={index}/>
-                )
-                })}
-                {orderItems && orderItems.map((item, index) => {
-                    return (
-                        <OrderItem 
-                            {...item}
-                            key={index} />
-                    )
-                })}
-                <OrderItem 
-                    item={'Total'}
-                    totalPrice={totalOrderPrice}/>
+    return (
+        <div className="takeaway-page-wrapper">
+            <p>Explanation: This is a page which i need for another project, but since the master branch of this project is currently bugged and i cant work there, im using this one and then will transfer my code.</p>
+            <h1>{orderStatus}</h1>
+            <h6>{subOrderStatus}</h6>
+            <ProgressBar status={orderStatus}/>
+            <div className="pizza-img">
+                <img src='../../src/assets/images/menu-pizzas.png'/>
             </div>
-        )
-    }
+            {orderDetails && orderDetails.map((orderDetail, index) => { 
+                return (
+                    <DetailComponent 
+                        {...orderDetail}
+                        key={index}/>
+            )
+            })}
+            {orderItems && orderItems.map((item, index) => {
+                return (
+                    <OrderItem 
+                        {...item}
+                        key={index} 
+                    />
+                )
+            })}
+            <OrderItem 
+                item='Total'
+                totalPrice={totalOrderPrice}
+            />
+            <OrderItem
+                item='Contacts to the restaurant'
+                subInfo='+359889990030'
+            />
+        </div>
+    )
+}
 
-    export default TakeawayPage
+export default TakeawayPage
