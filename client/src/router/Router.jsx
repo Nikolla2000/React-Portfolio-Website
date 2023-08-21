d9d32dedd2cd2c5ac2f0b7c0326b2fe94e6896b4
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-21 12:23:30 +0300

    Created Router.jsx

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,3 +1,8 @@
+import {createBrowserRouter} from "react-router-dom"
+
+import routes from "../utils/routes";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
 const Router = createBrowserRouter([
     {
         path: "/",
@@ -5,6 +10,9 @@ const Router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
             { index: true, element: routes.home.element},
+            { path: routes.about.path, element: routes.about.element},
+            { path: routes.work.path, element: routes.work.element},
+            { path: routes.contact.path, element: routes.contact.element},
         ]
     }
 ])
