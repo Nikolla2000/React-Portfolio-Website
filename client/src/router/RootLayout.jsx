1d23a2e43d4f556f94c5f62186d7c8ce1a8d203e
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-21 13:51:17 +0300

    Created the Root Layout Component

---
+++ client/src/router/RootLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet, useNavigate, useNavigation } from "react-router"
+import Navigation from "../Components/Layout/Navigation/Navigation"
+
+const RootLayout = () => {
+    const navigation  = useNavigation()
+    return (
+        <>
+        <Navigation />
+        <main>
+            {navigation.state === "loading" && (
+                <h2 className="loading-msg">Loading...</h2>
+            )}
+            <Outlet />
+        </main>
+        </>
+    )
+}
+
+export default RootLayout
