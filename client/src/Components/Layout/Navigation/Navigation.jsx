8325f32d6e982dbe8d65c7b6f30d6b34bf9a5a87
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-21 13:32:17 +0300

    Created the Navigation Component

---
+++ client/src/Components/Layout/Navigation/Navigation.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+import { Link, NavLink } from "react-router-dom"
+import routes from "../../../utils/routes";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const Navigation = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <Link to="/">
+                    <img src='../../../src/assets/images/waiting.png'/>
+                </Link>
+                <div className={showMobileMenu ? "show-mobile-menu" : "nav-menu"}>
+                    {Object.values(routes)
+                    .filter((element) => element.includeInNav === true)
+                    .map((element, index) => (
+                        <NavLink
+                            key={index + 1}
+                            to={element.path}
+                            className={({ isActive }) => (isActive && "active")}
+                        >
+                            {element.name}
+                        </NavLink>
+                    ))}
+                </div>
+            </div>
+            <div className="mobile-menu-wrapper">
+                <FontAwesomeIcon
+                    className="mobile-menu-btn"
+                    icon={faBars}
+                    oncClick={() => {
+                        setShowMobileMenu(!showMobileMenu);
+                    }}
+                 />
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
