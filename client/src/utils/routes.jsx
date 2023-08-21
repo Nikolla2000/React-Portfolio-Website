0c9752d153802f064880911e83209150bd1a4478
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-21 12:19:30 +0300

    Created the Routes object in the Utils folder

---
+++ client/src/utils/routes.jsx
@@ -0,0 +1,40 @@
+import HomePage from "../Pages/HomePage/HomePage";
+import AboutPage from "../Pages/AboutPage/AboutPage";
+import WorkPage from "../Pages/WorkPage/WorkPage";
+import ContactPage from "../Pages/ContactPage/ContactPage";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
+const routes = {
+    home: {
+        path: "/",
+        element: <HomePage/>,
+        name: "home",
+        includeInNav: true,
+    },
+    about: {
+        path: "/about",
+        element: <AboutPage/>,
+        name: "about",
+        includeInNav: true,
+    },
+    work: {
+        path: "/work",
+        element: <WorkPage/>,
+        name: "work",
+        includeInNav: true,
+    },
+    contact: {
+        path: "/contact",
+        element: <ContactPage/>,
+        name: "contact",
+        includeInNav: true,
+    },
+    errorPage: {
+        path: "/error",
+        element: <ErrorPage/>,
+        name: "errorPage",
+        includeInNav: false,
+    }
+}
+
+export default routes
