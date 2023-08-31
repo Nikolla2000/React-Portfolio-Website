9f3e66358fa1d986272457ffc0a86439682772d0
Nikola Uzunov <nikollla2000@abv.bg>
2023-08-31 15:11:45 +0300

    Setup Carousel Boilerplate for showing my projects

---
+++ client/src/Components/Portfolio/PortfolioSection.jsx
@@ -0,0 +1,36 @@
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from '../../../src/assets/images/menu-pizzas.png';
+
+const PortfolioSection = () => {
+    return (
+        <section className="portfolio-section">
+            <Carousel>
+      <Carousel.Item>
+        <img src={ExampleCarouselImage} text="First slide" />
+        <Carousel.Caption>
+          <h3>Fullstack Pizzarie Website</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={ExampleCarouselImage} text="First slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src={ExampleCarouselImage} text="First slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+        </section>
+    );
+};
+
+export default PortfolioSection;
--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import AboutSection from "../../Components/About/AboutSection"
 import ContactSection from "../../Components/Contact/ContactSection"
 import Header from "../../Components/Header/Header"
+import PortfolioSection from "../../Components/Portfolio/PortfolioSection"
 
 const HomePage = () => {
     return (
         <div className="homepage-wrapper">
             <Header/>
             <AboutSection/>
+            <PortfolioSection/>
             <ContactSection/>
         </div>
     )
