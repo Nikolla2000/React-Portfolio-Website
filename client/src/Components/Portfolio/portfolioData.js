ba2ccaad8ded63994f13de57e125ff6cdc21bb84
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-01 11:09:08 +0300

    Added the images of my other projects

--- client/src/Components/Portfolio/portfolioData.js
+++ client/src/Components/Portfolio/portfolioData.js
@@ -7,12 +7,18 @@ const portfolioData = [
         link: ""
     },
     {
-        imgPath: "../../../src/assets/images/react.png",
+        imgPath: "../../../src/assets/images/project2.png",
         title: "React CRUD App",
         description: "Fetched data from an API displayed in a list of products with CRUD operations"
     },
     {
-        imgPath: "../../../src/assets/images/message.png",
+        imgPath: "../../../src/assets/images/project3.mp4",
+        title: "Editable Movie Database",
+        description: "Fetched data from an API displayed in a list of products with CRUD operations",
+        video: true
+    },
+    {
+        imgPath: "../../../src/assets/images/project4.webp",
         title: "Calculator",
         description: "Vanilla Javascript Calculator with 4 basic operations"
     },
