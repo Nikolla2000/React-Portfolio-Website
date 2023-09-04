4fe406ce33aa61568e821b7a5675716573b5be27
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-04 21:40:57 +0300

    Set up a download button which downloads my CV as pdf when clicking

--- client/src/Components/CV/CvSection.jsx
+++ client/src/Components/CV/CvSection.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const CvSection = () => {
     return (
@@ -12,16 +12,24 @@ const CvSection = () => {
                     </Col>
                     <Col lg={6} md={6}>
                         <div className="about-details-wrapper">
-                            <h2>I Create Products Not Just Art</h2>
+                            <h2>I Code Experiences, Not Just Websites</h2>
                             <p>My name is Nikola Uzunov and I'm 23 years-old based in Varna, Bulgaria.
                                 I'm taking my first steps as a web developer. My primary focus is into frontend development.
                                  My appetite for growth and improvement drives me to explore this field further. 
                                  Let's connect and create amazing digital experiences together!</p>
                                  <ul>
-                                    <li>Point 1</li>
-                                    <li>Point 2</li>
-                                    <li>Point 3</li>
+                                    <li>Passionate and Aspiring Web Developer</li>
+                                    <li>Crafting Digital Solutions</li>
+                                    <li>Commitment to Excellence</li>
                                  </ul>
+                                 <Button variant="danger">
+                                    <a
+                                        href="/Nikola'sResume.pdf"
+                                        download="Nikola'sResume.pdf"  
+                                        className="download-button">
+                                        Download PDF
+                                    </a>
+                                 </Button>
                         </div>
                     </Col>
                 </Row>
