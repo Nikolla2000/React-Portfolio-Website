6682e136b89fbcf38aefdba81885362f947e9e04
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-06 21:15:19 +0300

    Created the Skills Section in the About Page
    Created the skills section similar to the one in the About Tabs in the Home Page but this time in the About Page. I'm using a carousel

--- client/src/Pages/AboutPage/skills/SkillsSection.jsx
+++ client/src/Pages/AboutPage/skills/SkillsSection.jsx
@@ -1,20 +1,24 @@
 import Carousel from 'react-bootstrap/Carousel';
 import { dividedSkillsData } from '../../../Components/About/Tabs/skills/skillsData';
 import { CarouselItem } from 'react-bootstrap';
+import "./SkillsSectionStyles.scss"
 
 
 const SkillsSection = () => {
     return (
         <section className="skills-section-about">
-            <h2>My Skills</h2>
+            <h2 className="about-header">My Skills</h2>
             <Carousel>
             {dividedSkillsData.map((dataSet, index) => (
                 <CarouselItem key={index + 1}>
-                    {dataSet.map((skill, index) => (
-                        <div className="skill-wrapper" key={index + 1}>
-                            <img src={skill.skillImg} alt='Skill Image'/>
-                        </div>
-                    ))}
+                    <div className="all-skills-wrapper">
+                        {dataSet.map((skill, index) => (
+                            <div className="skill-wrapper" key={index + 1}>
+                                <img src={skill.skillImg} alt='Skill Image'/>
+                                <span className='skill-name'>{skill.skillName}</span>
+                            </div>
+                        ))}
+                    </div>
                 </CarouselItem>
             ))}
             </Carousel>
