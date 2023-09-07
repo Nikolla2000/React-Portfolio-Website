b6c9e624b325b155e9ddb3ac2ba1789bffb2051a
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-07 19:58:25 +0300

    Removed the old coppied button, decided to make my own

--- client/src/Pages/PortfolioPage/projects/ProjectComponent.jsx
+++ client/src/Pages/PortfolioPage/projects/ProjectComponent.jsx
@@ -1,17 +1,13 @@
+import {Link} from "react-router-dom"
 
 const ProjectComponent = ( {data} ) => {
     return (
         <div className="project-wrapper">
             <img src={data.alternateImgPath ? 
             data.alternateImgPath : data.imgPath} alt="Project image"/>
-            <div id="container">
-            <button class="learn-more">
-                <span class="circle" aria-hidden="true">
-                <span class="icon arrow"></span>
-                </span>
-                <span class="button-text">Learn More</span>
-            </button>
-            </div>
+            <Link to="/">
+                <span>Learn More</span>
+            </Link>
         </div>
     );
 };
