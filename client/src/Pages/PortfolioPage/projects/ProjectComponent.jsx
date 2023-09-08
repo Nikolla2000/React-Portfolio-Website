785ed66f6664c052619eb417439081c78b95590c
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-08 16:31:28 +0300

    Created the pretty button animation by myself

--- client/src/Pages/PortfolioPage/projects/ProjectComponent.jsx
+++ client/src/Pages/PortfolioPage/projects/ProjectComponent.jsx
@@ -1,13 +1,27 @@
 import {Link} from "react-router-dom"
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from "react";
 
 const ProjectComponent = ( {data} ) => {
+    const [isImgHovered, setIsImgHovered] = useState(false)
+
     return (
-        <div className="project-wrapper">
+        <div 
+        className="project-wrapper" 
+        onMouseEnter={() => setIsImgHovered(true)} 
+        onMouseLeave={() => setIsImgHovered(false)}>
             <img src={data.alternateImgPath ? 
-            data.alternateImgPath : data.imgPath} alt="Project image"/>
-            <Link to="/">
+                data.alternateImgPath 
+                : data.imgPath} alt="Project image"/>
+            {isImgHovered && (
+                <Link to="/">
+                <div className="icon">
+                    <FontAwesomeIcon icon={faAngleRight} style={{color: "#f1f4f8",}} />
+                </div>
                 <span>Learn More</span>
             </Link>
+            )}
         </div>
     );
 };
