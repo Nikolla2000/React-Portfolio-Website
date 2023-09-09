44f5c93264ccf2c60676a54b1851def298083175
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-09 22:08:15 +0300

    Set up the boilerplate for the single project page

--- client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
+++ client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
@@ -1,7 +1,20 @@
+import { useParams } from "react-router-dom";
+import portfolioData from "../../../Components/Portfolio/portfolioData";
+import { useEffect, useState } from "react";
+
 const SingleProjectPage = () => {
+    const { projectId } = useParams()
+    const [projectData, setProjectData] = useState(undefined)
+
+    useEffect(() => {
+        setProjectData(() =>  portfolioData.find((project) => projectId == project.id))
+    }, [projectId])
+    
     return (
-        <div>
-            Project
+        <div className="single-project-page-wrapper">
+            {projectData && (
+                <h1>{projectData.title}</h1>
+            )}
         </div>
     );
 };
