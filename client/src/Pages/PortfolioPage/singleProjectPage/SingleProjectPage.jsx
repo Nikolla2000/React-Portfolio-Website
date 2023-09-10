822ce34bcb2fa208431ec7c7ab2e72025f2af87a
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-10 16:20:35 +0300

    Changed the layout if the page

--- client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
+++ client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
@@ -8,43 +8,45 @@ const SingleProjectPage = () => {
     const [projectData, setProjectData] = useState(undefined)
 
     useEffect(() => {
-        setProjectData(() =>  portfolioData.find((project) => projectId == project.id))
+        setProjectData(portfolioData.find((project) => projectId == project.id))
     }, [projectId])
     
     return (
-        <div className="single-project-page-wrapper">
+        <>
             {projectData && (
-                
-                <Container>
-                    <Row>
-                        <Col>
-                            <div className="project-img-wrapper">
-                                {projectData.alternateImgPath ?
-                                <img src={projectData.alternateImgPath} alt={`${projectData.title} image`}/> 
-                                : <img src={projectData.imgPath} alt={`${projectData.title} image`}/>}
-                            </div>
-                        </Col>
-                        <Col>
-                            <h1>{projectData.title}</h1>
-                            <h4>Description</h4>
-                            <p>{projectData.detailedDescription}</p>
-                            <Button>Github</Button>
-                            <p>{projectData.skillsDemonstrated}</p>
-                            <h4>Key Features</h4>
-                            {projectData.keyFeatures && (
-                                <ul>
-                                    {Object.entries(projectData.keyFeatures).map(([name, feature], index) => (
-                                    <li key={index}>
-                                        <strong>{name}:</strong> {feature}
-                                    </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </Col>
-                    </Row>
-                </Container>
+                <div className="single-project-page-wrapper">
+                    <h1>{projectData.title}</h1>
+                    <div className="project-img-wrapper">
+                        {projectData.alternateImgPath ?
+                            <img src={projectData.alternateImgPath} alt={`${projectData.title} image`} /> 
+                            : <img src={projectData.imgPath} alt={`${projectData.title} image`} />}
+                    </div>
+                    
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h4>Description</h4>
+                                <p>{projectData.detailedDescription}</p>
+                                <Button>Github</Button>
+                                <p>{projectData.skillsDemonstrated}</p>
+                            </Col>
+                            <Col>
+                                <h4>Key Features</h4>
+                                {projectData.keyFeatures && (
+                                    <ul>
+                                        {Object.entries(projectData.keyFeatures).map(([name, feature], index) => (
+                                            <li key={index}>
+                                                <strong>{name}:</strong> {feature}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                )}
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             )}
-        </div>
+        </>
     );
 };
 
