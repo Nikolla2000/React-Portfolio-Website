91e854e75471a86ac98642e00f63e49c9eab7292
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-10 17:16:57 +0300

    Added most of the styling of the Single Project Page

--- client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
+++ client/src/Pages/PortfolioPage/singleProjectPage/SingleProjectPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import portfolioData from "../../../Components/Portfolio/portfolioData";
 import { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import "./SingleProjectStyles.scss";
 
 const SingleProjectPage = () => {
     const { projectId } = useParams()
@@ -15,23 +16,24 @@ const SingleProjectPage = () => {
         <>
             {projectData && (
                 <div className="single-project-page-wrapper">
-                    <h1>{projectData.title}</h1>
-                    <div className="project-img-wrapper">
-                        {projectData.alternateImgPath ?
-                            <img src={projectData.alternateImgPath} alt={`${projectData.title} image`} /> 
-                            : <img src={projectData.imgPath} alt={`${projectData.title} image`} />}
-                    </div>
-                    
+                    <div className="single-project-header">
+                        <h1>{projectData.title}</h1>
+                        <div className="project-img-wrapper">
+                            {projectData.alternateImgPath ?
+                                <img src={projectData.alternateImgPath} alt={`${projectData.title} image`} /> 
+                                : <img src={projectData.imgPath} alt={`${projectData.title} image`} />}
+                        </div>
+                    </div>   
                     <Container>
                         <Row>
                             <Col>
-                                <h4>Description</h4>
+                                <h4 className="mb-5">Description</h4>
                                 <p>{projectData.detailedDescription}</p>
+                                <p className="mt-5">{projectData.skillsDemonstrated}</p>
                                 <Button>Github</Button>
-                                <p>{projectData.skillsDemonstrated}</p>
                             </Col>
                             <Col>
-                                <h4>Key Features</h4>
+                                <h4 className="mb-5">Key Features</h4>
                                 {projectData.keyFeatures && (
                                     <ul>
                                         {Object.entries(projectData.keyFeatures).map(([name, feature], index) => (
