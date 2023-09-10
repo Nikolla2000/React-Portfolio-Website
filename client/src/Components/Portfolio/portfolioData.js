63242b6a4bc7105ab2b22ddefb72d1e6ee04efb7
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-10 15:41:32 +0300

    Added all the other keyFeatures to the portfolioData

--- client/src/Components/Portfolio/portfolioData.js
+++ client/src/Components/Portfolio/portfolioData.js
@@ -8,13 +8,19 @@ const portfolioData = [
         detailedDescription: "The Fullstack Pizzeria Website is a collaborative effort created during a Javascript Bootcamp. This dynamic and user-friendly website serves as an online platform for a fictional pizzeria, showcasing the team's proficiency in web development. It features an interactive menu, online ordering, real-time order tracking, sending a contact emails and a seamless user experience. The project highlights the use of modern web technologies and teamwork in building a comprehensive web application. It's using React for the frontend and ExpressJS for the backend. Explore the codebase on GitHub to see how we brought this delightful pizzeria experience to life.",
         skillsDemonstrated: "This project demonstrates the skills and knowledge acquired during the bootcamp, including HTML, CSS, JavaScript, React, NodeJS and teamwork in a collaborative development environment.",
         keyFeatures: {
-            productManagement: "Users can perform CRUD operations on a list of products, including creating, reading, updating, and deleting entries.",
-            detailedProductInfo: "Each product entry includes essential details, such as a name, description, and other pertinent information.",
-            intuitiveUI: "The app features a user-friendly interface with clear navigation and input forms for adding or editing product information.",
-            dataFetching: "The application fetches product data from a dummy JSON API, providing real-world data management experience.",
-            responsiveDesign: "Designed with responsiveness in mind, the app adapts to various screen sizes, ensuring accessibility on both desktop and mobile devices.",
-            enhancedLearning: "Building this CRUD app enhances understanding of React components, state management, and HTTP requests, serving as a valuable learning experience."
-        },
+            "Interactive Menu":
+              "Users can explore an interactive menu featuring a mouthwatering array of pizzas and other delectable items, complete with vivid images and detailed descriptions.",
+            "Online Ordering":
+              "Customers have the convenience of placing orders online, selecting their desired items, customizing preferences, and scheduling deliveries or pickups.",
+            "Real-time Order Tracking":
+              "The website incorporates a real-time order tracking system, allowing customers to monitor the progress of their orders from preparation to delivery.",
+            "Register/Login":
+              "Customers can create accounts or log in to access additional features, such as order history, personalized recommendations, and a streamlined checkout process.",
+            "Contact and Feedback":
+              "Customers can reach out to the pizzeria through the website, send inquiries, and provide feedback. The 'Contact Us' section offers a direct line of communication.",
+            "Responsive Design":
+              "The website is designed to be responsive, ensuring an appealing and functional experience across different devices."
+          },
         video: true,
         link: "https://github.com/advanceacademy/bootcamp-js-june-23"
     },
@@ -24,6 +30,18 @@ const portfolioData = [
         title: "React CRUD App",
         description: "Retrieved information from an API presented within a product catalog featuring CRUD functionalities.",
         detailedDescription: "The Product Management App is a React-based web application that interacts with the \"DummyJSON API\" to perform CRUD operations on a list of products. This user-friendly app allows you to view, create, edit, and delete product entries effortlessly. Each product is displayed with its name and description, and you have the flexibility to update or remove them as needed. This project demonstrates proficiency in React, API integration, and the implementation of essential CRUD functionalities, making it a valuable tool for managing product data with ease.",
+        keyFeatures: {
+            "Product Management":
+              "Users can perform CRUD operations on a list of products, including creating, reading, updating, and deleting entries.",
+            "Detailed Product Information":
+              "Each product entry includes essential details, such as a name, description, and other pertinent information.",
+            "Intuitive User Interface":
+              "The app features a user-friendly interface with clear navigation and input forms for adding or editing product information.",
+            "Data Fetching":
+              "The application fetches product data from a dummy JSON API, providing real-world data management experience.",
+            "Enhanced Learning":
+              "Building this CRUD app enhances understanding of React components, state management, and HTTP requests, serving as a valuable learning experience.",
+          },
         link: "https://github.com/Nikolla2000/React-crud-app"
     },
     {
@@ -33,6 +51,18 @@ const portfolioData = [
         title: "Editable Movie Database",
         description: "Fetched movie data from an API displayed in a list of products with CRUD operations",
         detailedDescription: "This web application harnesses the power of CRUD operations to manage a dynamic catalog of movies. It seamlessly interacts with external movie databases to fetch detailed information about films. Users can browse through an extensive list of movies, each presented with a wealth of data including title, synopsis, genre, release year, and more. It has a rating system, interactive trailers and commenting feature",
+        keyFeatures: {
+            "Movie Management":
+              "Users can perform CRUD operations on a list of movies, including creating, reading, updating, and deleting entries.",
+            "Rating System":
+              "The app includes a rating feature, enabling users to rate movies and share their opinions.",
+            "Trailer Playback":
+              "Each movie entry may include a trailer link, providing an immersive viewing experience.",
+            "Comment Section":
+              "Users can add comments to share thoughts, recommendations, or reviews about movies.",
+            "Engaging Movie Exploration":
+              "Whether users are cinephiles or casual viewers, this app offers an engaging platform for discovering and managing their favorite movies."
+          },
         video: true,
         link: "https://github.com/Nikolla2000/CRUD-Movie-App"
     },
@@ -42,6 +72,18 @@ const portfolioData = [
         title: "Calculator",
         description: "Vanilla Javascript Calculator with 4 basic operations",
         detailedDescription: "This web-based calculator is a versatile tool designed to handle fundamental arithmetic operations with ease. It's an essential utility for anyone requiring quick and accurate calculations. Powered by pure JavaScript, this calculator enables users to perform addition, subtraction, multiplication, and division effortlessly.",
+        keyFeatures: {
+            "Four Arithmetic Operations":
+              "The calculator supports the four core arithmetic operations: addition, subtraction, multiplication, and division. Users can easily switch between these functions to perform calculations.",
+            "User-Friendly Interface":
+              "The user interface is intuitive and straightforward. It features a classic layout with numeric keys, operator buttons, and a display screen that showcases both input and results.",
+            "Real-Time Feedback":
+              "As users input numbers and operators, the calculator provides real-time feedback, displaying the equation as it's entered. This allows for quick and accurate adjustments.",
+            "Clear and Reset Function":
+              "In case of errors or to start a new calculation, the calculator offers a clear and reset function. Users can easily clear the current input or reset the calculator to its initial state.",
+            "History":
+              "The calculator maintains a history of previous calculations, making it convenient to review or reuse past results. Users can scroll through their calculation history and select previous equations for reference or modification."
+        },
         link: "https://github.com/Nikolla2000/Calculator-Vanilla-JS"
     },
 ];
