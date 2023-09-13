ffdd59e78c2a06f03281952cbe1040fe11245800
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-13 10:02:52 +0300

    Finalized Contact Page Styling

--- client/src/Pages/ContactPage/FormSection.jsx
+++ client/src/Pages/ContactPage/FormSection.jsx
@@ -8,15 +8,15 @@ const FormSection = () => {
     return (
         <section className="form-section-contactPage">
             <Container>
-                <Col lg={23}>
-                    <h2>Lets Talk</h2> 
+                <Col lg={12}>
+                    <h2 className="contact-page-heading">Lets Talk</h2> 
                 </Col>
                 <Row>
                     <Col lg={6} md={12}>
                         <div className="contact-form">
-                            <h2>Get In Touch</h2>
+                            <h2 className="mb-5">Get In Touch</h2>
                             <FormBuilder configurations={inputConfigurations}/>
-                            <Button>Send Message</Button>
+                            <Button type="submit" form="contact-form">Send Message</Button>
                         </div>
                     </Col>
                     <Col lg={6} md={12}>
