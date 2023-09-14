10664480c7b6618eded9053cb642dac88268d687
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-14 12:08:11 +0300

    Added "Fade-in" animation to the Page Header Title

--- client/src/Components/PageHeader/PageHeaderComponent.jsx
+++ client/src/Components/PageHeader/PageHeaderComponent.jsx
@@ -2,12 +2,13 @@ import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import "./PageHeaderStyles.scss"
 import { Link } from "react-router-dom";
 import shapesData from "./shapesData";
+import "animate.css"
 
 const PageHeaderComponent = ({ heading }) => {
     return (
         <section className="page-header">
-            <h1>{heading.title}</h1>
-            <Breadcrumb>
+            <h1 className='animate__animated animate__fadeIn'>{heading.title}</h1>
+            <Breadcrumb className='animate__animated animate__fadeIn'>
             <Breadcrumb.Item href="#"><Link to="/">Home</Link></Breadcrumb.Item>
             <Breadcrumb.Item>
                 {heading.breadcrumb}
