538190605142e1c1a8e559b997133c0f1028f79b
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-14 11:39:25 +0300

    Wrote the code for the new map using Leaflet library and Mapbox map visual

--- client/src/Components/StreetMap/StreetMap.jsx
+++ client/src/Components/StreetMap/StreetMap.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Import leaflet CSS
 
 const StreetMap = () => {
-  const mapStyles = {
-    height: '400px',
-    width: '100%',
-  };
+    const position = [43.214, 27.914];
+    const mapboxAccessToken = 'pk.eyJ1Ijoibmlrb2xsYTIwMCIsImEiOiJjbG1peDJidTAwZDEwM2VveHpyZm1ra3p6In0.ZHlGSMZ3zOjNzu1ox3Pa6w';
+    const mapboxStyleId = 'nikolla200/clmix079w00a701qt5hl646ds';
 
-  const defaultCenter = {
-    lat: 40.7128,
-    lng: -74.0060,
-  };
-
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyAua5Cs38XIfp_aV7t7WE68DK8jGqVZf5w">
-      <GoogleMap mapContainerStyle={mapStyles} zoom={10} center={defaultCenter}>
-        {/* Add map markers, polygons, or other map components here */}
-      </GoogleMap>
-    </LoadScript>
-  );
+    return (
+      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+            url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${mapboxAccessToken}`}
+            attribution='&copy; <a href="https://www.mapbox.com/">Mapbox</a> contributors'
+            id={mapboxStyleId}
+        />
+        <Marker position={position}>
+          <Popup>
+            A sample marker with a popup.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
 }
 
 export default StreetMap;
