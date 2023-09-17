89668f238837f6fd57b6c93668859852eed77a8b
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-17 16:39:37 +0300

    Wrote contacts controller and added it into the contact router

--- server/controllers/Contacts.controller.js
+++ server/controllers/Contacts.controller.js
@@ -1,2 +1,59 @@
 const ContactEmail = require("../models/Contact-Email-Schema/Email.model");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors/bad-request")
+const sendEmail = require("./EmailSender")
+
+const sendContactEmail = async (req, res) => {
+  const { firstName, lastName, email, subject, message } = req.body;
+
+  if(!firstName || !lastName || !email || !subject || !message) {
+    throw new BadRequestError("Please fill all fields")
+  }
+
+  const newContactEmail = await ContactEmail.create({
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    subject: subject,
+    message: message,
+  });
+
+  if(!newContactEmail) {
+    res
+      .status(StatusCodes.EXPECTATION_FAILED)
+      .send("Your message failed to send. Please try again.")
+  }
+
+  res.status(StatusCodes.ACCEPTED).json({
+    mailContent: newContactEmail,
+    message: "Contact mail sended successfully!",
+  })
+
+  const recipient = "nikolla.uzunov@gmail.com";
+  const templateName = "contactTemplate";
+
+  sendEmail(
+    newContactEmail.firstName,
+    newContactEmail.lastName,
+    recipient,
+    null,
+    templateName,
+    newContactEmail.email,
+    newContactEmail.subject,
+    newContactEmail.message
+  );
+};
+
+const getAllContactEmails = async (req, res) => {
+  try {
+    const allEmails = await ContactEmail.find()
+    res.status(200).json(allEmails)
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = {
+  sendContactEmail,
+  getAllContactEmails
+}
--- server/routes/Contact.router.js
+++ server/routes/Contact.router.js
@@ -1,26 +1,10 @@
 const express = require('express');
-const router = express.Router()
-const ContactEmail = require('../models/Contact-Email-Schema/Email.model')
+const router = express.Router();
+const ContactEmail = require('../models/Contact-Email-Schema/Email.model');
+const { sendContactEmail, getAllContactEmails } = require("../controllers/Contacts.controller");
 
-router.get('/', async (req, res) => {
-  try {
-    const emails = await ContactEmail.find()
-    res.status(200).json(emails)
-  } catch (error) {
-    console.log('Error retrieving emails:', error);
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
+router.get('/', getAllContactEmails)
 
-router.post('/sendEmail', async (req, res) => {
-  try {
-    const email = new ContactEmail(req.body)
-    await email.save()
-    res.status(201).json(email)
-  } catch (error) {
-    console.log('Error sending email:', error);
-    res.status(500).json({ error: 'Internal Server Error' })
-  }
-})
+router.post('/sendEmail', sendContactEmail)
 
 module.exports = router;
