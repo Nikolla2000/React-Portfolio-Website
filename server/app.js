40fdad43840339012ccfb8463cf7d13a33b0a9a7
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-17 08:08:44 +0300

    Created basic 'get' and 'post' requests to test the contact router
    Tested with Postman

--- server/app.js
+++ server/app.js
@@ -9,10 +9,15 @@ const connectDB = require("./db/connect");
 //Port
 const port = process.env.PORT || 3000;
 
-//routers( example : const someRouter = require("./routes/some.router"))
+//routers
+const contactRouter = require('./routes/Contact.router')
 
+//
 app.use(express.static("./public"));
 app.use(express.json());
+
+//routes
+app.use('/contacts', contactRouter)
 
 const start = async () => {
   try {
