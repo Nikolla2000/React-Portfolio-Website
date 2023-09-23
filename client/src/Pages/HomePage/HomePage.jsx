a775eabf8ff7477551fed9d57be4dbdc13363b43
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-23 17:38:25 +0300

    Removed the ErrorPage test from the homepage

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -3,17 +3,15 @@ import ContactSection from "../../Components/Contact/ContactSection"
 import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
 import PortfolioSection from "../../Components/Portfolio/PortfolioSection"
-import ErrorPage from "../ErrorPage/ErrorPage"
 
 const HomePage = () => {
     return (
         <div className="homepage-wrapper">
-            {/* <Header/>
+            <Header/>
             <AboutSection/>
             {/* <PortfolioSection/> */}
-            {/* <ContactSection/>
-            <Footer/> */} 
-            <ErrorPage/>
+            <ContactSection/>
+            <Footer/>
         </div>
     )
 }
