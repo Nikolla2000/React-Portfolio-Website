487d42602ef3e74730f527eb7265db5d6278ee57
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-26 11:07:41 +0300

    Moved the images from the assets folder to public
    Moved the images to public for deployment in netlify since vite cant find them in the assets folder

--- client/src/Components/About/Tabs/services/servicesData.js
+++ client/src/Components/About/Tabs/services/servicesData.js
@@ -2,15 +2,15 @@ const servicesData = [
     {
         serviceName: "Web Development",
         description: "Focusing on front-end development, I excel at creating captivating user interfaces using HTML/CSS, JavaScript and React. While my expertise lies in front-end, I also possess a little backend skills with Node.js and Express for complementary functionality.",
-        img: '../../../src/assets/images/service1.png',
-        imgLight: '../../../src/assets/images/service1-light.png'
+        img: '/images/service1.png',
+        imgLight: '/images/service1-light.png'
 
     },
     {
         serviceName: "Web Design",
         description: "I create engaging visuals and user-friendly layouts. While my expertise is primarily in development, I take interest in making very appealing and beautiful web pages and products.",
-        img: '../../../src/assets/images/service2.png',
-        imgLight: '../../../src/assets/images/service2-light.png'
+        img: '/images/service2.png',
+        imgLight: '/images/service2-light.png'
 
     }, 
 ]
