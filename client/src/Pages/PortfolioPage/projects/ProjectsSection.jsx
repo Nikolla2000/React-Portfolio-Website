a3eb1328b5ae1bb4faf62a2d563d70c6e1703b84
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-27 16:23:56 +0300

    Added 'loading' to the projects in the portfolio page so on deployment they not appears blank when not loaded

--- client/src/Pages/PortfolioPage/projects/ProjectsSection.jsx
+++ client/src/Pages/PortfolioPage/projects/ProjectsSection.jsx
@@ -1,24 +1,42 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-import portfolioData from "../../../Components/Portfolio/portfolioData";
+import portfolioDataJS from "../../../Components/Portfolio/portfolioData";
 import ProjectComponent from "./ProjectComponent";
 import "./ProjectsSection.scss"
+import { useEffect, useState } from "react";
 
 
 const ProjectsSection = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [portfolioData, setPortfolioData] = useState(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setPortfolioData(portfolioDataJS);
+            setIsLoading(false);
+        }, 2000);
+    }, [])
     return (
         <section className="all-projects-wrapper">
             <Container>
                 <Row>
-                    {portfolioData.map((project) => (
-                        <Col key={project.imgPath} lg={6} md={6} xs={12}>
-                            <ProjectComponent data={project} />
-                        </Col>
-                    ))}
-                    <a href="https://github.com/Nikolla2000" 
-                        className="viewmore-button mt-5"
-                        target="_blank">
-                        <Button variant="danger">View more...</Button>
-                    </a>
+                    {isLoading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        <>
+                            {portfolioData.map((project) => (
+                                <Col key={project.imgPath} lg={6} md={6} xs={12}>
+                                    <ProjectComponent data={project} />
+                                </Col>
+                            ))}
+                            <a
+                                href="https://github.com/Nikolla2000"
+                                className="viewmore-button mt-5"
+                                target="_blank"
+                            >
+                                <Button variant="danger">View more...</Button>
+                            </a>
+                        </>
+                    )}
                 </Row>
             </Container>
         </section>
