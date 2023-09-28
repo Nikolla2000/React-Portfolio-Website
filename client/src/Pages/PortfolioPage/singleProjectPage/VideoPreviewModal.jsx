522d64e5f19d2680264482684755bf030ee52226
Nikola Uzunov <nikollla2000@abv.bg>
2023-09-28 10:11:00 +0300

    Added "Loading..." to the video modal

--- client/src/Pages/PortfolioPage/singleProjectPage/VideoPreviewModal.jsx
+++ client/src/Pages/PortfolioPage/singleProjectPage/VideoPreviewModal.jsx
@@ -1,22 +1,38 @@
+import { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
+import { Spinner } from 'react-bootstrap';
 
 
 const VideoPreviewModal = ({ video, show, setShow })  => {
-    const handleClose = () => setShow(false);
+  const [projectVideo, setProjectVideo] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const handleClose = () => setShow(false);
 
-    return (
-         <>
-      <Modal show={show} onHide={handleClose} className="video-modal" size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered>
-        <Modal.Header closeButton>
-        </Modal.Header>
-        <Modal.Body>
-            <video src={video} autoPlay loop muted className='modal-video'/>
-        </Modal.Body>
-      </Modal>
-    </>
-    );
+  useEffect(() => {
+    setTimeout(() => {
+      setProjectVideo(video)
+      setIsLoading(false)
+    }, 1000)
+  },[video])
+
+  return (
+        <>
+    <Modal show={show} onHide={handleClose} className="video-modal" size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered>
+      <Modal.Header closeButton>
+      </Modal.Header>
+      <Modal.Body>
+          {isLoading ? 
+          <div className="text-center">
+            <Spinner animation="grow" variant="danger" />
+            <p className="loading-text">Loading...</p>
+          </div> 
+          : <video src={projectVideo} autoPlay loop muted className='modal-video'/>}
+      </Modal.Body>
+    </Modal>
+  </>
+  );
 };
 
 export default VideoPreviewModal;
