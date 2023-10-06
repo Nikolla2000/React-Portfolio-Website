6830bf0efad8f553acc489511ebd663474696c1c
Nikola Uzunov <nikollla2000@abv.bg>
2023-10-06 16:10:42 +0300

    Added close icon from Material UI

--- client/src/Components/ChatBot/ChatBot.jsx
+++ client/src/Components/ChatBot/ChatBot.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChatBot from "react-simple-chatbot"
 import instructions from '../ChatBot/instructions';
+import CloseIcon from '@mui/icons-material/Close';
 import "./ChatBotStyles.scss"
 
 
 const Chat = () => {
+  const [closeChat, setCloseChat] = useState(false)
+  
   return (
-    <div className='chatbot-wrapper'>
+    <div className={`chatbot-wrapper ${closeChat && 'remove'}`}>
       <ChatBot
         steps={instructions}/>
+        <CloseIcon onClick={() => setCloseChat(true)}/>
     </div>
   );
 };
